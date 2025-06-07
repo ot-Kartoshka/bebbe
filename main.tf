c85a2699f0f73857c6966d6558475d80dcde976f
Kartoshka <42836104+ot-Kartoshka@users.noreply.github.com>
2025-06-08 02:19:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
   endpoints {
-    s3       = "http://localhost:4566"
-    lambda   = "http://localhost:4566"
-    iam      = "http://localhost:4566"
-    sqs      = "http://localhost:4566"
+    s3       = "http://172.31.24.189:4566"
+    lambda   = "http://172.31.24.189:4566"
+    iam      = "http://172.31.24.189:4566"
+    sqs      = "http://172.31.24.189:4566"
   }
 }
 
