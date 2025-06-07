45a2d2e287e20cca2cb6041db3cdbc82edf091f0
Kartoshka <42836104+ot-Kartoshka@users.noreply.github.com>
2025-06-08 02:40:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,18 +11,9 @@ terraform {
 
 provider "aws" {
   region                      = "eu-central-1"
-  access_key                  = "test"
-  secret_key                  = "test"
   s3_use_path_style           = true
-  skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  endpoints {
-    s3       = "http://172.31.24.189:4566"
-    lambda   = "http://172.31.24.189:4566"
-    iam      = "http://172.31.24.189:4566"
-    sqs      = "http://172.31.24.189:4566"
-  }
 }
 
 
